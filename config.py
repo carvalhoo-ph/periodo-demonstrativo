28ced6c7a71a37b7472ba502f335bd48d6cc1ab1
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-01-24 22:03:56 -0300

    Criação 01

---
+++ config.py
@@ -0,0 +1,4 @@
+rds_host = "portal-ex-colab.cn2i04a6agxy.us-east-1.rds.amazonaws.com"
+db_username = "postgres"
+db_password = "Koda020116"
+db_name = "portal-ex-colab"
---
+++ lambda_function.py
@@ -0,0 +1,46 @@
+import json
+import psycopg2
+from config import rds_host, db_username, db_password, db_name
+
+def lambda_handler(event, context):
+    cpf = event['cpf']
+    
+    # Conexão com o banco de dados
+    connection = psycopg2.connect(
+        host=rds_host,
+        user=db_username,
+        password=db_password,
+        dbname=db_name
+    )
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT ano, mes FROM periodo_demonstrativo WHERE cpf = %s"
+            cursor.execute(sql, (cpf,))
+            result = cursor.fetchall()
+        
+        # Agrupar meses por ano
+        grouped_result = {}
+        for row in result:
+            ano, mes = row
+            if ano not in grouped_result:
+                grouped_result[ano] = []
+            grouped_result[ano].append(mes)
+        
+        # Formatar a resposta
+        response_data = [{"ano": ano, "meses": meses} for ano, meses in grouped_result.items()]
+        response = {
+            'statusCode': 200,
+            'body': json.dumps(response_data)
+        }
+        
+    except Exception as e:
+        response = {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+    
+    finally:
+        connection.close()
+    
+    return response
